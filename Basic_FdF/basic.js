40a0b03d42da8de69b2142911e7e2ef146369b48
defaziofederico <defaz86@gmail.com>
2019-10-08 10:17:46 +0200

    basic1

---
+++ Basic_FdF/basic.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$("#present").on('click', function() {
+$(this).text("You clicked on the Present box")});
+$("#present1").on('click', function() {
+$(this).text("You clicked on the Santa Claus")});
+$("#present2").on('click', function() {
+$(this).text("You clicked on the Christmas bell")});
+});
