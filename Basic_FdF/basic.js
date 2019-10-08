eae7685fba67902bb20ed8ca17e4d3eb11afc24d
defaziofederico <defaz86@gmail.com>
2019-10-08 11:26:54 +0200

    basic2

--- Basic_FdF/basic.js
+++ Basic_FdF/basic.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-$("#present").on('click', function() {
-$(this).text("You clicked on the Present box")});
-$("#present1").on('click', function() {
-$(this).text("You clicked on the Santa Claus")});
-$("#present2").on('click', function() {
-$(this).text("You clicked on the Christmas bell")});
+// $(document).ready(function(){
+// $("#present").on('click', function() {
+// $(this).text("You clicked on the Present box")});
+// $("#present1").on('click', function() {
+// $(this).text("You clicked on the Santa Claus")});
+// $("#present2").on('click', function() {
+// $(this).text("You clicked on the Christmas bell")});
+// });
+
+
+$(document).ready(function() {
+for (var i = 0; i < 9; i++) {
+$("body").append("<div>H</div");	
+
+$("div").addClass("santa")
+$("div").addClass("gift")
+$(".gift").append("<img src='https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878'>");
+$("<img src='https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878'>").appendTo($(".gift"));
+
+
 });
