34d7bf475a1d08aaec14d190923f63f68346f727
sebichiotoroiu <sebichiotoroiu@yahoo.com>
2019-10-08 13:54:33 +0200

    Basic2.html

--- Basic_FdF/basic.js
+++ Basic_FdF/basic.js
@@ -9,13 +9,20 @@
 
 
 $(document).ready(function() {
-for (var i = 0; i < 9; i++) {
-$("body").append("<div>H</div");	
+for (var i = 0; i < 3; i++) {
+	$("body").append("<div></div");	
+	$("div").addClass("santa")
+	$("div").addClass("gift")
+	$(".gift").prepend("<img src='https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878' width='100'>");
+	$("<img src='https://juststickers.in/wp-content/uploads/2016/11/santa-claus.png' width='100'>").appendTo($(".santa"));
 
-$("div").addClass("santa")
-$("div").addClass("gift")
-$(".gift").append("<img src='https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878'>");
-$("<img src='https://cdn.shopify.com/s/files/1/1369/2605/articles/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.png?v=1511752878'>").appendTo($(".gift"));
-
-
+};
+$('.santa').dblclick(function() {
+        $(this).hide()
+        });
 });
+
+$('.div').click(function() {
+        $(this).slideUp()
+        });
+        
